e8bc58569f82e88de7e57849d9b10c63e31b065d
taitamfc <taitam8590@gmail.com>
2022-10-25 14:21:59 +0700

    Update ServiceInterface.php

--- app/Services/Interfaces/ServiceInterface.php
+++ app/Services/Interfaces/ServiceInterface.php
@@ -5,7 +5,7 @@ interface ServiceInterface
 {
     public function all($request);
     public function find($id);
-    public function create($request);
+    public function store($request);
     public function update($request, $id);
     public function destroy($id);
 }
