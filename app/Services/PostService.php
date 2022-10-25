924e083978801c718f91d64b9e603cd76767eea3
taitamfc <taitam8590@gmail.com>
2022-10-25 14:08:41 +0700

    complete service setup

---
+++ app/Services/PostService.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Services;
+
+// use App\Repositories\Interfaces\PostInterface;
+use App\Services\Interfaces\PostServiceInterface;
+
+class PostService implements PostServiceInterface
+{
+    /* Triển khai các phương thức trong PostServiceInterface */
+    public function all($request){
+        echo __METHOD__;
+    }
+    public function find($id){
+        echo __METHOD__;
+    }
+    public function create($request){
+        echo __METHOD__;
+    }
+    public function update($request, $id){
+        echo __METHOD__;
+    }
+    public function destroy($id){
+        echo __METHOD__;
+    }
+}
